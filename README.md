9fb00237f1d3d89f8ef2669e2235daee2f0050ef
Lupey <lupey.dev@proton.me>
2026-04-26 22:40:02 -0500

    added settings for autoclicker

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 This is an auto clicker I made currently only in *python working on other languages
 
 If you are on linux you will have to run sudo/root because it hooks your keyboard to enable the auto clicker
+
+## Click Settings
+To use the click settings change the values of enable and disable in clicksettings.json to corresponding keybinds
--- autoclicker.py
+++ autoclicker.py
@@ -1,18 +1,25 @@
 import keyboard
 import time
 from pynput.mouse import Button, Controller
+import json
 
 mouse = Controller()
+
+with open("clicksettings.json", "r") as file:
+    data = json.load(file)
+
+enable_btn = data["enable"]
+disable_btn = data["disable"]
 
 click_active = False
 
 while True:
-    if keyboard.is_pressed("q"):
+    if keyboard.is_pressed(enable_btn):
         click_active = True
         print("clicker enabled")
         time.sleep(0.15)
 
-    if keyboard.is_pressed("e"):
+    if keyboard.is_pressed(disable_btn):
         click_active = False
         print("clicker disabled")
         time.sleep(0.15)
